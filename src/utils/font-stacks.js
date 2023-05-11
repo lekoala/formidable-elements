c5fd4cb0414e99bebab38a2a69e7c0f0af199ff2
Thomas <thomas@lekoala.be>
2023-05-11 16:02:55 +0200

    tweak icons

---
+++ src/utils/font-stacks.js
@@ -0,0 +1,17 @@
+// @link https://modernfontstacks.com/
+export const SystemUI = "system-ui, sans-serif";
+export const Transitional = "Charter, 'Bitstream Charter', 'Sitka Text', Cambria, serif";
+export const OldStyle = "'Iowan Old Style', 'Palatino Linotype', 'URW Palladio L', P052, serif";
+export const Humanist = "Seravek, 'Gill Sans Nova', Ubuntu, Calibri, 'DejaVu Sans', source-sans-pro, sans-serif";
+export const GeometricHumanist = "Avenir, Montserrat, Corbel, 'URW Gothic', source-sans-pro, sans-serif";
+export const ClassicalHumanist = "Optima, Candara, 'Noto Sans', source-sans-pro, sans-serif";
+export const NeoGrotesque = "Inter, Roboto, 'Helvetica Neue', 'Arial Nova', 'Nimbus Sans', Arial, sans-serif";
+export const MonospaceSlabSerif = "'Nimbus Mono PS', 'Courier New', monospace";
+export const MonospaceCode = "ui-monospace, 'Cascadia Code', 'Source Code Pro', Menlo, Consolas, 'DejaVu Sans Mono', monospace";
+export const Industrial = "Bahnschrift, 'DIN Alternate', 'Franklin Gothic Medium', 'Nimbus Sans Narrow', sans-serif-condensed, sans-serif";
+export const RoundedSans =
+  "ui-rounded, 'Hiragino Maru Gothic ProN', Quicksand, Comfortaa, Manjari, 'Arial Rounded MT', 'Arial Rounded MT Bold', Calibri, source-sans-pro, sans-serif";
+export const SlabSerif = "Rockwell, 'Rockwell Nova', 'Roboto Slab', 'DejaVu Serif', 'Sitka Small', serif";
+export const Antique = "Superclarendon, 'Bookman Old Style', 'URW Bookman', 'URW Bookman L', 'Georgia Pro', Georgia, serif";
+export const Didone = "Didot, 'Bodoni MT', 'Noto Serif Display', 'URW Palladio L', P052, Sylfaen, serif";
+export const Handwritten = "'Segoe Print', 'Bradley Hand', Chilanka, TSCu_Comic, casual, cursive";
